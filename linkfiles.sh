1e7593c4dfa0a5815d5b5bb51fbfa1968f03b60c
Zach Whaley <zachbwhaley@gmail.com>
2014-01-27 15:21:54 -0600

    That last commit didn't work at all...

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Returns 0 if the 1st arg is found in the other args; 1 otherwise.
+contains () {
+    for e in "${@:2}"; do
+        [[ ${e} == ${1} ]] && return 0;
+    done
+    return 1
+}
+
 # A list of files to exclude from symoblic linking
-exclude=("README.md" "linkfiles.sh")
+exclude=( "README.md" "linkfiles.sh" )
 gitdir=$(pwd)
 
 for file in $(git ls-files); do
-    for e in $exclude; do
-        if [[ ! $e == $file ]]; then
-            ln -fTs $gitdir/$file ~/$file
-        fi
-    done
+    if ! contains $file ${exclude[@]}; then
+        ln -fTs $gitdir/$file ~/$file
+    fi
 done
