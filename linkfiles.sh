c8267619a442128aaa8b573752adbe91426d7bf2
Zach Whaley <zachbwhaley@gmail.com>
2014-02-19 21:06:12 -0600

    This was weird, so I made it better
    Didn't realize by using git ls-files, I was linking every file inside each directory.
    
    I've changed it to like only the files/directories in the root of the dotfiles repo, excluding .git, README.md, etc.

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# Returns 0 if the 1st arg is found in the other args; 1 otherwise.
-contains () {
-    for e in "${@:2}"; do
-        [[ ${e} == ${1} ]] && return 0;
-    done
-    return 1
-}
+# A "|" separated list of files to exclude from symoblic linking
+exclude=".git|README.md|linkfiles.sh"
 
-# A list of files to exclude from symoblic linking
-exclude=( "README.md" "linkfiles.sh" )
-
-for file in $(git ls-files); do
-    if ! contains $file ${exclude[@]}; then
-        mkdir -p ~/$(dirname $file)
-        ln -vfTs $(pwd)/$file ~/$file
-    fi
+for file in $(ls -A | egrep -v $exclude); do
+    ln -vfs $(pwd)/$file ~/$file
 done
