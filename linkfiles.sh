96ef3275d0b6323b8277a543845b49df7b7c1cca
Zach Whaley <zachbwhaley@gmail.com>
2014-02-20 07:26:45 -0600

    Better regexp for grep'n

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # A "|" separated list of files to exclude from symoblic linking
-exclude=".git\>|README.md|linkfiles.sh"
+exclude="\b(\.git|README\.md|linkfiles\.sh)\b"
 
 for file in $(ls -A | egrep -v $exclude); do
     ln -vfs $(pwd)/$file ~/$file
