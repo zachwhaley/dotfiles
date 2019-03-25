3a0da02d61a8cdb9cb6086659f59d61a984e3778
Zachary Whaley <zachbwhaley@gmail.com>
2019-03-25 11:05:53 -0500

    Add pyenv settings to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,8 @@ alias tm 'tmux -2'
 
 # Enable fzf key bindings
 fzf_key_bindings
+
+# Pyenv
+set -x PATH "/home/zacharyw/.pyenv/bin" $PATH
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
