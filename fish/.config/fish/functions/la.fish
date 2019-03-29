2e3cf9ce8a0f7a3fe4dac2b4f80f5e74632e71f6
Zachary Whaley <zachbwhaley@gmail.com>
2019-03-29 11:59:17 -0500

    Add la and lla functions

---
+++ fish/.config/fish/functions/la.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 2
+function la --description 'List contents of directory, including hidden files in directory'
+	ls -a $argv
+end
---
+++ fish/.config/fish/functions/lla.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function lla --description 'Lists contents of directory, including hidden files using long format'
+	ll -a $argv
+end
