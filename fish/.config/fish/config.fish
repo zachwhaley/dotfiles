7edff4fc32b95799de9b23fcfed343c25c545787
Zachary Whaley <zachbwhaley@gmail.com>
2019-04-01 10:20:51 -0500

    omf install pyenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,8 +3,3 @@ alias tm tmux
 
 # Enable fzf key bindings
 fzf_key_bindings
-
-# Pyenv
-set -x PATH "/home/zacharyw/.pyenv/bin" $PATH
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
